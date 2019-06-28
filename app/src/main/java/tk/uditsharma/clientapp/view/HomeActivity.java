770fe1c619590f00c39af91bb968c3db65f18f95
uditshrma <udit.sharma261@gmail.com>
2019-06-28 18:48:32 +0530

    Improved Home Activity UI.

--- app/src/main/java/tk/uditsharma/clientapp/view/HomeActivity.java
+++ app/src/main/java/tk/uditsharma/clientapp/view/HomeActivity.java
@@ -1,33 +1,22 @@
 package tk.uditsharma.clientapp.view;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
 import tk.uditsharma.clientapp.R;
 import tk.uditsharma.clientapp.RegisterActivity;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
-        TextView welcomeMsg = (TextView)findViewById(R.id.userWelcome);
-        welcomeMsg.setText("Welcome " + getIntent().getExtras().getString("current_user"));
-
-    }
-	
-	public void showAllUsers2(View view){
-        Intent listIntent = new Intent(this, UserListActivity.class);
-        startActivity(listIntent);
-    }
-
-    public void goToWishList2(View view){
-        Intent listIntent = new Intent(this, WishListActivity.class);
-        startActivity(listIntent);
+        setContentView(R.layout.home_layout);
+        TextView welcomeMsg = (TextView) findViewById(R.id.un_text);
+        welcomeMsg.setText(getIntent().getExtras().getString("current_user"));
     }
 
     public void showAllUsers(View view){
